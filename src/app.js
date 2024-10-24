7e419ce8178ce31363d0773e504415b61c3bbe5a
Domingo Ramírez Morales <128168900+Rzmsdo@users.noreply.github.com>
2024-10-24 13:51:37 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function frase(partesfrase) {
+    return partesfrase[Math.floor(Math.random() * partesfrase.length)];
+  }
+
+  let genaratorAI = document.querySelector("#datos");
+  genaratorAI.innerHTML =
+    frase(who) + " " + frase(action) + " " + frase(what) + " " + frase(when);
 };
+// // const frase = partesfrase => {
+//   let construyendo = "";
+//   construyendo = partesfrase[Math.floor(Math.random()) * partesfrase.length];
+
+// //   return construyendo;
+//      };
